90532d1a27da68d50734f26848a32c777676f248
Akshat-Jain <akjn11@gmail.com>
2019-05-09 14:01:57 +0530

    Added task 4: Storing data using Postgres Database

---
+++ task4_storing_data_in_postgres/script.py
@@ -0,0 +1,41 @@
+import psycopg2
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+	return render_template('index.html')
+
+@app.route('/submit', methods=['GET', 'POST'])
+def submit():
+	if request.method == 'POST':
+		name = request.form['name']
+		subject = request.form['subject']
+		marks = request.form['marks']
+
+	# fo = open("marks.txt", "a")
+	# fo.writelines(name)
+	# fo.writelines(' ')
+	# fo.writelines(subject)
+	# fo.writelines(' ')
+	# fo.writelines(marks)
+	# fo.writelines('\n')
+	# fo.close()
+	conn = psycopg2.connect(
+			host="localhost",
+			database="sample_db",
+			user="postgres",
+			password="postgres"
+		)
+	c=conn.cursor()
+	c.execute('CREATE TABLE IF NOT EXISTS marks (name TEXT, subject TEXT, marks INTEGER)')
+	c.execute('INSERT INTO marks (name, subject, marks) VALUES (%s, %s, %s)',
+		(name,subject,marks))
+	conn.commit()
+	c.close()
+	conn.close()
+	return render_template('complete.html')
+
+
+if __name__ == "__main__":
+    app.run()
